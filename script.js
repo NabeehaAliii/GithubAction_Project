8008c1ffa325f18c8354c64b50aff93e0f362ceb
Nabeeha Ali <69257476+Nabeehaali2004@users.noreply.github.com>
2024-10-05 20:02:13 +0500

    Added Resume Files

---
+++ script.js
@@ -0,0 +1,26 @@
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Scroll Animation for Fade-In Sections with scale
+const sections = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible');
+        }
+    });
+});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
